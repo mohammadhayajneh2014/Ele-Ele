713386653ff284feeb98a49846afec6311698899
mohammad ahmad theeb hayajneh <mohammadhayagneh96@gmail.com>
2021-03-29 00:07:27 +0300

    first electrical elemant

---
+++ java/java.js
@@ -0,0 +1,63 @@
+//alert("email is (mohammad)");
+
+//var email = prompt('enter your email');
+//alert("age is (25)");
+//var age = prompt('enter your age');
+//if (email == "mohammad") {
+//    document.write("your email is correct");
+//}
+//else {
+
+  //  document.write("your email is wrong");
+//}
+//if (age == "25") {
+  //  document.write("your age is correct");
+//}
+//else {
+
+  //  document.write("your age is wrong");
+//}
+
+
+//document.write('<h3>'+email+'</h3>');
+ 
+//document.write('<h3>'+age+'</h3>');
+
+alert("your password is mohammad");
+var Password = prompt('git your password');
+var password = 'mohammad';
+while(Password !== password){ 
+   Password = prompt('Please enter password')
+}
+
+alert("YOUR Elemants IS bus,cab,ins");
+
+
+
+var favElemants = prompt('Whats your fav elemants?');
+while (favElemants !== "bus" && favElemants !== "cab" && favElemants !== "ins") {
+favElemants = prompt('Whats your fav elemants?');
+  
+} 
+console.log(favElemants)
+// m4,m16
+var Elemants = prompt('How many Elemants would you like to see?');
+console.log(Elemants)
+/// number
+
+
+var image = ' '; 
+
+
+for(var i = 1 ; i <=Elemants; i++ ){
+  if(favElemants === 'bus'){
+      image = image + '<img src="photo/electrical-bus-bars.jpg"/>';
+  }else if(favElemants === 'cab'){
+      image = image + '<img src="photo/electrical-cables.jpg"/>';
+  }else if(favElemants === 'ins'){
+      image = image + '<img src="photo/insulators.jpg"/>';
+  }
+}
+console.log(image);
+document.write(image);
+
